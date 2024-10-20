0b58d2b073c4976f8d13ca12dee3a3f63fb6b4b6
zhangyu2234 <46471060+zhangyu2234@users.noreply.github.com>
2024-10-20 15:50:34 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import argparse
 from process.utils import dataloader, train, test
 from model.CNN import Conv_1d
+from model.CNN_attention import *
 import torch
 import pickle
 
@@ -17,11 +18,12 @@ parser.add_argument('--dropout', type=float, default=0.5, help='Dropout')
 parser.add_argument('--in_channel', type=int, default=1, help='in_channel')
 parser.add_argument('--weight_decay', type=float, default=0.000005, help='Weight decay')
 parser.add_argument('--n_class', type=int, default=4, help='Classes')
+parser.add_argument('--reduction', type=int, default=2, help='Reduce channel')
+parser.add_argument('--model_type', type=str, default='CNN', help='choice model')
 
 args = parser.parse_args()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Conv_1d(args.in_channel, args.dropout, args.n_class)
 
 with open(args.data_path, 'rb') as f:
     data, labels = pickle.load(f)
@@ -29,8 +31,13 @@ f.close()
 
 train_loader, test_loader, val_loader = dataloader(data, labels)
 
+if args.model_type == 'CNN':
+    model = Conv_1d(args.in_channel, args.dropout, args.n_class)
+elif args.model_type == 'SE_Conv':
+    model = SE_Conv(args.in_channel, args.dropout, args.reduction, args.n_class)
 
 train(train_loader, val_loader, model, args.num_epoch, args.lr, args.weight_decay, args.patience, device, args.weight_path)
 test(model, args.weight_path, test_loader, device)
+
 
 
