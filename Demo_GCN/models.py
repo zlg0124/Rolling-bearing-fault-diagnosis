02b5ba1595e5a8da52d553837cda0e9726849b3b
Joker <46471060+zhangyu2234@users.noreply.github.com>
2024-11-08 20:40:40 +0800

    Add files via upload

---
+++ Demo_GCN/models.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layer import *
+
+
+"""
+原始代码中, hidden1=64, hidden2=128, hidden3=64
+"""
+class Reconstruction(nn.Module):
+    def __init__(self, in_dim, dropout=0.):
+        super().__init__()
+        self.construct = InnerProductDecoder(in_dim, dropout)
+    
+    def forward(self, x):
+        return self.construct(x)
+
+
+class Encoder(nn.Module):
+    def __init__(self, in_features, hidden1, hidden2, dropout=0., bias=True):
+        super().__init__()
+
+        self.layer1 = GraphConvoluationSparse(in_features, hidden1, dropout, bias)
+        self.layer2 = GraphConvolution(hidden1, hidden2, dropout, bias)
+        self.recons = Reconstruction(hidden2)
+    
+
+    def forward(self, x, adj):
+        # x -> 2708, 1433
+        hidden = self.layer1(x, adj)
+        embeddings = self.layer2(hidden, adj) # self.embeddings -> 2708, 128
+        # self.noise = torch.randn(self.embeddings.shape, device=x.device)
+        reconstructions = self.recons(embeddings)
+        return embeddings, reconstructions
+    
+
+class Generator_z2g(nn.Module):
+    def __init__(self, in_features, hidden1, hidden2, dropout=0., bias=True):
+        super().__init__()
+        # in_features: 特征矩阵的维度
+
+        self.layer1 = GraphConvolution(hidden2, hidden1, dropout, bias)
+
+        self.layer2 = GraphConvolution(hidden1, in_features, dropout, bias)
+
+    def forward(self, x, adj):
+        hidden = self.layer1(x, adj)
+        embeddings = self.layer2(hidden, adj)
+        return embeddings
+
+
+class D_graph(nn.Module):
+    def __init__(self, in_features, hidden2):
+        super().__init__()
+        self.linear1 = nn.Linear(in_features, 512)
+        self.linear2 = nn.Linear(512, hidden2)
+        self.linear3 = nn.Linear(hidden2, 1)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        hidden = self.relu(self.linear1(x))
+        hidden_1 = self.relu(self.linear2(hidden))
+        out = self.linear3(hidden_1)
+        return out
+
+class Discriminator(nn.Module):
+    def __init__(self, in_dim, hidden1, hidden3):
+        super().__init__()
+        # 原始的features经过Encoder -> 2708, 1433 -> 2708, hidden2
+        self.linear1 = nn.Linear(in_dim, hidden3)
+        self.linear2 = nn.Linear(hidden3, hidden1)
+        self.linear3 = nn.Linear(hidden1, 1)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        hidden_1 = self.relu(self.linear1(x))
+        hidden_2 = self.relu(self.linear2(hidden_1))
+        out = self.linear3(hidden_2)
+        return out
+
+
+if __name__ == '__main__':
+    model_z2g = Generator_z2g(1433, 64, 128)
+    adj = torch.randn(2708, 2708)
+    x = torch.randn(2708, 128)
+    print(model_z2g(x, adj).shape)
+  
+
+
+    
+
+
+
+
+
+    
